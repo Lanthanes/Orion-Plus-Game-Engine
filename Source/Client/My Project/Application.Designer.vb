20120f656060e50d32b46ddf651f1d4d000e0a1d
Damian666 <p-puk999@hotmail.com>
2017-07-05 16:07:14 +0200

    Switched back to .net 4

--- Source/Client/My Project/Application.Designer.vb
+++ Source/Client/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.OrionClient.FrmMenu
+            Me.MainForm = Global.OrionClient.frmMenu
         End Sub
     End Class
 End Namespace
